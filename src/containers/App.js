7d490e36b8ddc59fc2713cfa854e393873ead86e
melipi <melissapita@gmail.com>
2021-04-27 21:34:19 -0400

    initial commit of hooks + redux implementation

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,22 +1,30 @@
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import { members } from '../members';
+import { setSearchField } from '../actions';
 import './App.css';
 
 function App() {
+    const dispatch = useDispatch();
+    const { searchField } = useSelector((state) => state);
     const [cardInfo] = useState(members);
-    const [searchField, setSearchField] = useState(''); 
 
     const onSearchChange = (event) => {
-        setSearchField(event.target.value)       
+        dispatch(setSearchField(event.target.value))       
     }
 
     const filteredMembers = members.filter(member => 
         member.name.toLowerCase().includes(searchField.toLowerCase()))
 
     return !cardInfo.length ?
-        <h1>Loading...</h1> :
+        <div className='vh-100 dt w-100'>
+            <div className='dtc v-mid tc white ph3 ph4-l'>
+            <h1 className='f6 f2-m f-subheadline-l fw6 tc'>Loading...</h1>
+            </div>
+        </div>
+        :
             (
                 <div className='tc pa4'>
                     <img 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
+import { searchLoona } from './reducers';
 import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(searchLoona)
+
+ReactDOM.render(
+
+                <Provider store={store}>
+                    <App />
+                </Provider>, 
+                document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
